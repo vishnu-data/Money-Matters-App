27ceafbe7788eddf1c512bd11394943a226193b1
[VISHNU'S-PC] <[vinu0infinity@gmail.com]>
2024-11-19 07:15:52 +0530

    final commit

---
+++ app/src/main/java/com/example/myapplication/ExpenseDao.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+import androidx.room.*
+
+@Dao
+interface ExpenseDao {
+
+    @Query("SELECT * FROM expense_table WHERE  amount= :amount")
+    suspend fun getExpenseByAmount(amount: String): Expense?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertExpense(items: Expense)
+
+    @Update
+    suspend fun updateExpense(items: Expense)
+
+    @Delete
+    suspend fun deleteExpense(items: Expense)
+}
